a369bb6d72d0586e13015677416eaf0d7c9f446b
Ilnicki010 <dominik.ilnicki@gmail.com>
2020-03-27 23:00:11 +0100

    rewrite using react-map-gl mapbox wrapper for react

--- src/components/MapComponent/MapComponent.jsx
+++ src/components/MapComponent/MapComponent.jsx
@@ -1,58 +1,44 @@
 import React from "react";
-import mapboxgl from "mapbox-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
 
 import * as turf from "@turf/turf";
-
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY;
+import styles from "./MapComponent.module.scss";
 
 class MapComponent extends React.Component {
-  componentDidMount() {
-    const { userLocation, places } = this.props;
-    this.map = new mapboxgl.Map({
-      container: this.mapContainer,
-      style: "mapbox://styles/mapbox/streets-v11",
-      center: [userLocation.long, userLocation.lat],
+  state = {
+    viewport: {
+      width: "100%",
+      height: "40vh",
       zoom: 10,
-    });
-    this.map.on("load", () => {
-      places.map((place) => {
-        new mapboxgl.Marker()
-          .setLngLat([place.geometry.location.lng, place.geometry.location.lat])
-          .addTo(this.map);
-      });
-      const center = turf.point([userLocation.long, userLocation.lat]);
-      const radius = 10;
-      const options = {
-        steps: 20,
-        units: "kilometers",
-      };
-
-      const circle = turf.circle(center, radius, options);
-      this.map.addLayer({
-        id: "circle-outline",
-        type: "line",
-        source: {
-          type: "geojson",
-          data: circle,
-        },
-        paint: {
-          "line-color": "blue",
-          "line-opacity": 0.3,
-          "line-width": 4,
-          "line-offset": 5,
-        },
-      });
-    });
-  }
+    },
+  };
 
   render() {
+    const { viewport } = this.state;
+    const { userLocation, places } = this.props;
     return (
       <div>
-        <div
-          ref={(el) => {
-            this.mapContainer = el;
-          }}
-        />
+        <ReactMapGL
+          {...viewport}
+          latitude={userLocation.lat}
+          longitude={userLocation.long}
+          mapStyle="mapbox://styles/mapbox/light-v9"
+          onViewportChange={(viewportChange) =>
+            this.setState({ viewport: viewportChange })
+          }
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_KEY}
+        >
+          {places.map((place) => (
+            <Marker
+              latitude={place.geometry.location.lat}
+              longitude={place.geometry.location.lng}
+              offsetLeft={-20}
+              offsetTop={-10}
+            >
+              <div className={styles.marker} />
+            </Marker>
+          ))}
+        </ReactMapGL>
       </div>
     );
   }
