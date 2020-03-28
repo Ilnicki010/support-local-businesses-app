4117f2433a64bc426c733edcbc602e0a5e6b8628
Ilnicki010 <dominik.ilnicki@gmail.com>
2020-03-28 11:26:27 +0100

    autocomplete input

--- src/views/HomeView/HomeView.jsx
+++ src/views/HomeView/HomeView.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 
+import styles from "./HomeView.module.scss";
+import LocationInput from "../../components/LocationInput/LocationInput";
+
 class HomeView extends React.Component {
+  state = {
+    searchQuery: {
+      location: null,
+    },
+  };
+
+  getLocation = (latlng) => {
+    this.setState({
+      searchQuery: {
+        location: latlng,
+      },
+    });
+  };
+
   render() {
-    return <h1>Home view</h1>;
+    return (
+      <main>
+        <span>Search results for bussiness in locatioon</span>
+        <div>
+          <LocationInput getLatLng={(latlng) => this.getLocation(latlng)} />
+          <input type="text" />
+        </div>
+        <div className={styles.contentWrapper} />
+      </main>
+    );
   }
 }
 export default HomeView;
