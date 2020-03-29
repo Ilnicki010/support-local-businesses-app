107863d73e88b2cb9ad6b83838da6a638296fa64
Ilnicki010 <dominik.ilnicki@gmail.com>
2020-03-29 15:11:00 +0200

    finish filters

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,58 +1,31 @@
 import React, { Component } from "react";
-import Checkbox from "../Checkbox/Checkbox";
+import Select from "react-select";
+
+import styles from "./Filters.module.scss";
 
 class Filters extends Component {
-  componentWillMount = () => {
-    this.selectedCheckboxes = new Set();
-    this.props.filterList.forEach(item => {
-      if (item.selected) this.selectedCheckboxes.add(item.value);
+  state = {
+    selectedOption: null,
+  };
+
+  handleChange = (selectedOption) => {
+    this.setState({ selectedOption }, () => {
+      console.log(`Option selected:`, this.state.selectedOption);
+      this.props.filteredValuesHandler(selectedOption);
     });
-    this.filteredValuesHandler();
   };
-
-  toggleCheckbox = label => {
-    if (this.selectedCheckboxes.has(label)) {
-      this.selectedCheckboxes.delete(label);
-    } else {
-      this.selectedCheckboxes.add(label);
-    }
-    this.filteredValuesHandler();
-  };
-
-  // send a list of CSV filters back to the callback
-  filteredValuesHandler = () => {
-    const selectedArray = [];
-    this.selectedCheckboxes.forEach(item => selectedArray.push(item));
-    this.props.filteredValuesHandler(selectedArray.join(","));
-  };
-
-  handleFormSubmit = formSubmitEvent => {
-    formSubmitEvent.preventDefault();
-
-    for (const checkbox of this.selectedCheckboxes) {
-      console.log(checkbox, "is selected.");
-    }
-  };
-
-  createCheckboxes = () => this.props.filterList.map(this.createCheckbox);
-
-  createCheckbox = checkboxObj => (
-    <Checkbox
-      label={checkboxObj.display}
-      handleCheckboxChange={this.toggleCheckbox}
-      value={checkboxObj.value}
-      key={checkboxObj.value}
-      selected={checkboxObj.selected || false}
-    />
-  );
 
   render() {
+    const { selectedOption } = this.state;
+
     return (
-      <div className="filters-container">
-        <div className="filters-header-row">Filter by:</div>
-        <div className="filters-row">
-          <div className="filters-col">{this.createCheckboxes()}</div>
-        </div>
+      <div>
+        <Select
+          className={styles.filter}
+          value={selectedOption}
+          onChange={this.handleChange}
+          options={this.props.filterList}
+        />
       </div>
     );
   }
--- src/components/LocationInput/LocationInput.jsx
+++ src/components/LocationInput/LocationInput.jsx
@@ -2,22 +2,23 @@ import React from "react";
 
 import PlacesAutocomplete, {
   geocodeByAddress,
-  getLatLng
+  getLatLng,
 } from "react-places-autocomplete";
 import styles from "./LocationInput.module.scss";
 
 class LocationInput extends React.Component {
   state = { address: "" };
 
-  handleChange = address => {
+  handleChange = (address) => {
     this.setState({ address });
   };
 
-  handleSelect = address => {
+  handleSelect = (address) => {
+    this.setState({ address });
     geocodeByAddress(address)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => this.props.getLocationInfo(latLng, address))
-      .catch(error => console.error("Error", error));
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => this.props.getLocationInfo(latLng, address))
+      .catch((error) => console.error("Error", error));
   };
 
   render() {
@@ -28,17 +29,23 @@ class LocationInput extends React.Component {
         onChange={this.handleChange}
         onSelect={this.handleSelect}
       >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div style={{ position: "relative" }}>
+        {({
+          getInputProps,
+          suggestions,
+          getSuggestionItemProps,
+          loading,
+          label,
+        }) => (
+          <div className={styles.inputWrapper}>
             <input
               {...getInputProps({
                 placeholder: "Search Places ...",
-                className: "location-search-input"
+                className: "location-search-input",
               })}
             />
             <div className={styles.autocompleteDropdown}>
-              {loading && <div>Loading...</div>}
-              {suggestions.map(suggestion => {
+              {loading && <div>Load(suggestion)</div>}
+              {suggestions.map((suggestion) => {
                 const className = suggestion.active
                   ? styles.suggestionItemActive
                   : styles.suggestionItem;
@@ -46,7 +53,7 @@ class LocationInput extends React.Component {
                 return (
                   <div
                     {...getSuggestionItemProps(suggestion, {
-                      className
+                      className,
                     })}
                   >
                     <span>{suggestion.description}</span>
