5b12c4d69114464ad6ccd4d7965efa50625d3f25
David Wertheimer <david@wertheimer.com>
2020-03-28 18:42:26 -0700

    Adding key prop to <li> to get rid of Warning

--- src/components/BusinessesList/BusinessesList.jsx
+++ src/components/BusinessesList/BusinessesList.jsx
@@ -3,13 +3,13 @@ import styles from "./BusinessesList.module.scss";
 import Button from "../Button/Button";
 
 const BusinessesList = ({ listOfPlaces }) => {
-  const style = (photoRef) => ({
-    backgroundImage: `url(https://maps.googleapis.com/maps/api/place/photo?photoreference=${photoRef}&sensor=false&maxheight=1600&maxwidth=1600&key=${process.env.REACT_APP_GOOGLE_API_KEY})`,
+  const style = photoRef => ({
+    backgroundImage: `url(https://maps.googleapis.com/maps/api/place/photo?photoreference=${photoRef}&sensor=false&maxheight=1600&maxwidth=1600&key=${process.env.REACT_APP_GOOGLE_API_KEY})`
   });
   return (
     <ul>
-      {listOfPlaces.map((placeObject) => (
-        <li className={styles.listElement}>
+      {listOfPlaces.map((placeObject, i) => (
+        <li className={styles.listElement} key={i}>
           <div
             className={styles.image}
             style={
