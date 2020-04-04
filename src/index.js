a0788be22853df3b27c3103b562e5aeee0a154b8
Ilnicki010 <dominik.ilnicki@gmail.com>
2020-04-04 21:32:06 +0200

    add sentry error tracker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
 import "./index.css";
 import App from "./App";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_URL,
+});
 
 ReactDOM.render(
   <React.StrictMode>
