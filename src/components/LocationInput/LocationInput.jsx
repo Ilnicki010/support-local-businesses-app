d594adfe07707b9cf5d2d6c235c7ef0efdfefb41
Ilnicki010 <dominik.ilnicki@gmail.com>
2020-04-26 15:39:03 +0200

    hooks

--- src/components/LocationInput/LocationInput.jsx
+++ src/components/LocationInput/LocationInput.jsx
@@ -1,98 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng,
 } from "react-places-autocomplete";
-import { ReactComponent as GpsIcon } from "../../assets/gps.svg";
 
 import Button from "../Button/Button";
 import styles from "./LocationInput.module.scss";
 
-class LocationInput extends React.Component {
-  state = { address: "" };
+function LocationInput({ getLocationInfo }) {
+  const [address, setAddress] = useState("");
 
-  getUserLocation = () => {
-    console.log("ere");
-    this.setState({ address: "near your" });
+  const getUserLocation = () => {
+    setAddress("Near you");
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position, err) => {
-        console.log(position);
-
-        this.props.getLocationInfo(
+        getLocationInfo(
           { lat: position.coords.latitude, lng: position.coords.longitude },
-          this.state.address
+          address
         );
       });
     } else {
-      console.log("error");
+      // error
     }
   };
 
-  handleChange = (address) => {
-    this.setState({ address });
-  };
-
-  handleSelect = (address) => {
-    this.setState({ address });
+  const handleSelect = (addressFromInput) => {
+    setAddress(addressFromInput);
     geocodeByAddress(address)
       .then((results) => getLatLng(results[0]))
-      .then((latLng) => this.props.getLocationInfo(latLng, address))
+      .then((latLng) => getLocationInfo(latLng, addressFromInput))
       .catch((error) => console.error("Error", error));
   };
 
-  render() {
-    return (
-      <div className={styles.wrapper}>
-        <PlacesAutocomplete
-          value={this.state.address}
-          searchOptions={{
-            types: ["geocode"],
-          }} /* restricts to places rather than business names */
-          onChange={this.handleChange}
-          onSelect={this.handleSelect}
-        >
-          {({
-            getInputProps,
-            suggestions,
-            getSuggestionItemProps,
-            loading,
-            label,
-          }) => (
-            <div className={styles.inputWrapper}>
-              <input
-                {...getInputProps({
-                  placeholder: "Set location...",
-                  className: "location-search-input",
-                  autoFocus: true,
-                })}
-              />
+  return (
+    <div className={styles.wrapper}>
+      <PlacesAutocomplete
+        value={address}
+        searchOptions={{
+          types: ["geocode"],
+        }} /* restricts to places rather than business names */
+        onChange={(value) => setAddress(value)}
+        onSelect={handleSelect}
+      >
+        {({
+          getInputProps,
+          suggestions,
+          getSuggestionItemProps,
+          loading,
+          label,
+        }) => (
+          <div className={styles.inputWrapper}>
+            <input
+              {...getInputProps({
+                placeholder: "Set location...",
+                className: "location-search-input",
+                autoFocus: true,
+              })}
+            />
 
-              <div className={styles.autocompleteDropdown}>
-                {loading && <div>Load(suggestion)</div>}
-                {suggestions.map((suggestion) => {
-                  const className = suggestion.active
-                    ? styles.suggestionItemActive
-                    : styles.suggestionItem;
+            <div className={styles.autocompleteDropdown}>
+              {loading && <div>Load(suggestion)</div>}
+              {suggestions.map((suggestion) => {
+                const className = suggestion.active
+                  ? styles.suggestionItemActive
+                  : styles.suggestionItem;
 
-                  return (
-                    <div
-                      {...getSuggestionItemProps(suggestion, {
-                        className,
-                      })}
-                    >
-                      <span>{suggestion.description}</span>
-                    </div>
-                  );
-                })}
-              </div>
+                return (
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
             </div>
-          )}
-        </PlacesAutocomplete>
-        <Button secondary onClick={() => this.getUserLocation()}>
-          Near me
-        </Button>
-      </div>
-    );
-  }
+          </div>
+        )}
+      </PlacesAutocomplete>
+      <Button secondary onClick={() => getUserLocation()}>
+        Near me
+      </Button>
+    </div>
+  );
 }
 export default LocationInput;
