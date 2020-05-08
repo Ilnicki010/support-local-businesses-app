c0290eeb2c96fa58cc6fa2a2a86abc860e54a10f
Sidhaant Anand <=>
2020-05-08 14:46:32 +0530

    Syntax fix

--- server/helpers/fetch_airtable_id_from_place.js
+++ server/helpers/fetch_airtable_id_from_place.js
@@ -1,6 +1,6 @@
 module.exports = function fetchIdFromPlaceId(place_id,airtable_records) {
   return new Promise(function (resolve, reject) {
-    for (record of records) {
+    for (record of airtable_records) {
       if (record.place_id == place_id) {
         resolve(record.id);
         return;
